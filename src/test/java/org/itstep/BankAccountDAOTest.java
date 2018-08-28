8dac15352362970882f3d13edacd6a02dd144828
Lenovo <Lenovo@DESKTOP-UAOUHUM>
2018-08-28 23:58:44 +0300

    123

---
+++ src/test/java/org/itstep/BankAccountDAOTest.java
@@ -0,0 +1,107 @@
+package org.itstep;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class BankAccountDAOTest {
+
+	private BankAccount bankAccount;
+	
+	@Before
+	public void setUp() throws Exception {
+		BankAccount bankAccount = new BankAccount("Alex", "Pupkin", "(099)999-99-99", "pupkin@ukr.net", 1000.0, 500.0, 500.0);
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		bankAccountDAO.saveBankAccount(bankAccount);
+	}
+
+	@Ignore
+	@Test
+	public void testSaveBankAccount() {
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		BankAccount result = bankAccountDAO.getBankAccount(bankAccount.getId());
+		assertNotNull(result);
+		assertEquals("Alex", result.getFirstName());
+		assertEquals("Pupkin", result.getSecondName());
+		assertEquals("(099)999-99-99", result.getTelephone());
+		assertEquals("pupkin@ukr.net", result.getEmail());
+		assertEquals(Integer.valueOf("1000.0"), result.getCarrency());
+		assertEquals(Integer.valueOf("500.0"), result.getAmount());
+		assertEquals(Integer.valueOf("500.0"), result.getAmountOfCredit());
+		
+	}
+	@Ignore
+	@Test
+	public void testUpdateBankAccount() {
+		BankAccount bankAccount1 = new BankAccount("Alex", "Petrov", "(066)333-99-11", "petrov@gmail.com", 1600.0, 400.0, 700.0);
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		bankAccountDAO.updateBankAccount(bankAccount1);
+		BankAccount result = bankAccountDAO.getBankAccount(bankAccount.getId());
+		assertEquals("Alex", result.getFirstName());
+		assertEquals("Petrov", result.getSecondName());
+		assertEquals("(066)333-99-11", result.getTelephone());
+		assertEquals("petrov@gmail.com", result.getEmail());
+		assertEquals(Integer.valueOf("1600.0"), result.getCarrency());
+		assertEquals(Integer.valueOf("400.0"), result.getAmount());
+		assertEquals(Integer.valueOf("700.0"), result.getAmountOfCredit());
+				
+	}
+	@Ignore
+	@Test
+	public void testGetBankAccount() {
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		BankAccount result = bankAccountDAO.getBankAccount(bankAccount.getId());
+		assertNotNull(result);
+		assertEquals("Alex", result.getFirstName());
+		assertEquals("Pupkin", result.getSecondName());
+		assertEquals("(099)999-99-99", result.getTelephone());
+		assertEquals("pupkin@ukr.net", result.getEmail());
+		assertEquals(Integer.valueOf("1000.0"), result.getCarrency());
+		assertEquals(Integer.valueOf("500.0"), result.getAmount());
+		assertEquals(Integer.valueOf("500.0"), result.getAmountOfCredit());
+				
+	}
+@Test
+	public void testDeleteBankAccount() {
+	BankAccountDAO bankAccountDAO = new BankAccountDAO();
+	bankAccountDAO.deleteBankAccount(bankAccount);
+	BankAccount result = bankAccountDAO.getBankAccount(bankAccount.getId());
+	assertNull(result);
+	}
+	@Ignore
+	@Test
+	public void testGetBankAccountByFirstNameAndSecondName() {
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		BankAccount result = bankAccountDAO.getBankAccountByFirstNameAndSecondName(bankAccount.getFirstName(), bankAccount.getSecondName());
+		assertEquals("Alex", result.getFirstName());
+		assertEquals("Pupkin", result.getSecondName());
+			}
+	@Ignore
+	@Test
+	public void testGetBankAccountByTelephone() {
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		List<BankAccount> result = (List<BankAccount>) bankAccountDAO.getBankAccountByTelephone(bankAccount.getTelephone());
+		for (BankAccount bankAccountArray : result) {
+		assertEquals("(099)999-99-99", bankAccountArray.getTelephone());
+		}
+	}
+	@Ignore
+	@Test
+	public void testGetStudentToSecondName() {
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		List<BankAccount> result = bankAccountDAO.getStudentToSecondName(bankAccount.getSecondName());
+		for (BankAccount bankAccountArray : result) {
+			assertEquals("Pupkin", bankAccountArray.getSecondName());
+		}
+	}
+	@After
+	public void tearDown() throws Exception {
+		BankAccountDAO bankAccountDAO = new BankAccountDAO();
+		bankAccountDAO.deleteBankAccount(bankAccount);
+	}
+}
